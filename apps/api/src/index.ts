214b9065469869ead53c5e4c2ad93f301ea55da1
Muhammad Maruf <maruuf82@gmail.com>
2026-05-02 13:43:42 +0700

    refactor: reorganize imports and update CORS settings in index.ts; clean up .env.example and .example.env

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,16 +1,24 @@
-import { Hono } from 'hono';
-import { clerekHandler } from './features/clerek/handler.js';
-import { storeHandler } from './features/store/handler.js';
-import { authHandler } from './features/auth/handler.js';
-import { errorHandler, notFoundHandler } from './error.js';
-import { healthHandler } from './features/health/handler.js';
+import { Hono } from "hono";
+import { clerekHandler } from "./features/clerek/handler.js";
+import { storeHandler } from "./features/store/handler.js";
+import { authHandler } from "./features/auth/handler.js";
+import { errorHandler, notFoundHandler } from "./error.js";
+import { healthHandler } from "./features/health/handler.js";
+import { cors } from "hono/cors";
 
 const app = new Hono();
-
-app.route('/', clerekHandler);
-app.route('/auth', authHandler);
-app.route('/store', storeHandler);
-app.route('/health', healthHandler);
+app.use(
+  "/*",
+  cors({
+    origin: "http://localhost:5173",
+    allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+  }),
+);
+app.route("/", clerekHandler);
+app.route("/auth", authHandler);
+app.route("/store", storeHandler);
+app.route("/health", healthHandler);
 app.onError(errorHandler);
 app.notFound(notFoundHandler);
 export default app;
